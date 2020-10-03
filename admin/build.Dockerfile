b03274cfc3cbc6870da987234d23df684b9f3db6
khoale193 <khoale.193@gmail.com>
2020-10-03 09:12:55 +0700

    admin deploy

--- admin/build.Dockerfile
+++ admin/build.Dockerfile
@@ -8,9 +8,9 @@ RUN mkdir $APP_HOME
 WORKDIR $APP_HOME
 
 CMD echo BUILDING... \
-  && npm install --no-color --silent --prefix mlm-ng-admin/client \
-  && npm run build --progress=false --no-color --prefix mlm-ng-admin/client \
-  && cd mlm-ng-admin \
+  && npm install --no-color --silent --prefix admin-source/client \
+  && npm run build --progress=false --no-color --prefix admin-source/client \
+  && cd admin-source \
   && echo COMPRESSING... \
   && tar -czf $APP_HOME/build/admin.tar.gz server \
   && echo DONE!!!
--- release/run.sh
+++ release/run.sh
@@ -8,17 +8,17 @@ case $1 in
     ;;
     "admin")
     docker-compose --project-name=$BUILD_ID-mlm-standard up --no-color --no-start --build $1
-    docker stop $(docker ps -f name=vkids_$1 -q)
+    docker stop $(docker ps -f name=mlm-standard_$1 -q)
     docker-compose --project-name=$BUILD_ID-mlm-standard start $1
     ;;
     "member")
     docker-compose --project-name=$BUILD_ID-mlm-standard up --no-color --no-start --build $1
-    docker stop $(docker ps -f name=vkids_$1 -q)
+    docker stop $(docker ps -f name=mlm-standard_$1 -q)
     docker-compose --project-name=$BUILD_ID-mlm-standard start $1
     ;;
     "resource")
     docker-compose --project-name=$BUILD_ID-mlm-standard up --no-color --no-start --build $1
-    docker stop $(docker ps -f name=vkids_$1 -q)
+    docker stop $(docker ps -f name=mlm-standard_$1 -q)
     docker-compose --project-name=$BUILD_ID-mlm-standard start $1
     ;;
     *) echo please provide a service name ;;

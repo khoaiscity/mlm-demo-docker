226ea6b9d11f7d6acb66793e8a5f041559fdc5a8
khoaiscity <khoale.193@gmail.com>
2020-12-21 14:41:02 +0700

    be

--- release/run.sh
+++ release/run.sh
@@ -2,9 +2,9 @@
 
 case $1 in
     "backend")
-    docker-compose --project-name=$BUILD_ID-mlm-standard up --no-color --no-start --build $1
-    docker stop $(docker ps -f name=mlm-standard_$1 -q)
-    docker-compose --project-name=$BUILD_ID-mlm-standard start $1
+    docker-compose --project-name=$BUILD_ID-mlm-demo up --no-color --no-start --build $1
+    docker stop $(docker ps -f name=mlm-demo_$1 -q)
+    docker-compose --project-name=$BUILD_ID-mlm-demo start $1
     ;;
     "admin")
     docker-compose --project-name=$BUILD_ID-mlm-standard up --no-color --no-start --build $1
